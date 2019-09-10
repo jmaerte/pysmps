69b0e04af380fe96cf2376ad97e2178e4eb79afa
Julian Maerte <jmaerte@mathematik.uni-marburg.de>
2019-09-10 11:15:29 +0200

    added 2-stage linear program support and sto file parsing for indep and blocks

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smpspy',
+    version='0.1',
+    description='Script for parsing SMPS files.',
+    author='Julian Maerte',
+    author_email='maertej@students.uni-marburg.de',
+    packages=find_packages(),
+)
